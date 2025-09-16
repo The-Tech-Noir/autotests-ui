8caf85d30258395ce6aeac557183bf6f2699e9ea
The-Tech-Noir <nicky.lerner@gmail.com>
2025-09-16 20:07:28 +0300

    5.5 Углубление в Playwright. Практика по углубленной работе с Playwright

--- playwright_disabled.py
+++ playwright_disabled.py
@@ -7,7 +7,5 @@ with sync_playwright() as playwright:
 
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
-
     login_button = page.get_by_test_id('login-page-login-button')
     expect(login_button).to_be_disabled()
